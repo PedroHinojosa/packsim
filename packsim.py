0e75c3183d41541ae7fb852f53f0d48793a1eb77
PedroHinojosa <petehinojosa85@gmail.com>
2021-07-07 01:51:18 -0500

    created name generator

--- packsim.py
+++ packsim.py
@@ -1,2 +1,23 @@
-print("Hello")
-print("testing");
+import random
+
+def playerNameGen():
+    firstNames = ["Dequarius", "Lamar", "Lonzo", "Kunta", "Ezekial", "Charles", "Carmelon", "Kobus", "Elvin", "Julius", "Dolph"]
+    lastNames =["Carter", "Smiff", "Johnson", "Porter", "Ali", "Byrd", "Wilkins", "Morris", "Paul", "Cole", "Miller"]
+
+    ranFName=firstNames [random.randint(0,10)]
+    ranLName=lastNames [random.randint(0,10)]
+
+    playerName= ranFName + " " + ranLName
+
+    return playerName
+
+numCards = 5
+
+cardsInPack = []
+
+for i in range(0,numCards):
+    getName = playerNameGen()
+    cardsInPack.insert(i, getName)
+
+print (cardsInPack)
+

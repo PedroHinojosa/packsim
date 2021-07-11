81698a83d7d280d5afa30ded81f6575dd2b85334
PedroHinojosa <petehinojosa85@gmail.com>
2021-07-10 20:29:33 -0500

    Added collection and menu system. TODO added to bottom of code

--- packsim.py
+++ packsim.py
@@ -1,8 +1,8 @@
 import random
+import copy
 
-
-def playerNameGen():
-    firstNames = ["Dequarius", "Lamar", "Lonzo", "Kunta", "Ezekial", "Charles", "Carmelon", "Kobus", "Elvin", "Julius", "Dolph"]
+def setPlayerName():
+    firstNames = ["Dequarius", "Lamar", "Lonzo", "Kunta", "Ezekiel", "Charles", "Carmelon", "Kobus", "Elvin", "Julius", "Dolph"]
     lastNames =["Carter", "Smiff", "Johnson", "Porter", "Ali", "Byrd", "Wilkins", "Morris", "Paul", "Cole", "Miller"]
 
     ranFName=firstNames [random.randint(0,10)]
@@ -12,26 +12,89 @@ def playerNameGen():
 
     return playerName
 
-def setFoilStatus():
-    fStatus="Non-Foil"
-    if random.randint(0,10) == 10:
-        fStatus="Foil"
-    return fStatus
+def setCardStatus():
+    cStatus="Error! No Card Status Assigned!"
+    ranNumGen=random.randint(0,100)
+    if ranNumGen >50 and ranNumGen <91:
+        cStatus="Insert"
+    else:
+        if ranNumGen>90 and ranNumGen<100:
+            cStatus="Foil"
+        else:
+            if ranNumGen==100:
+                cStatus="Auto"
+            else:
+                cStatus="Base"
+    return cStatus
 
-def setAutoStatus():
-    aStatus="Non-Auto"
-    if random.randint(0,100) == 100:
-        aStatus="Auto"
-    return aStatus
+def collectionAdd(existingCollection, cardsToAdd):
+    
+    d=dict()
 
-case_list=[]
+    for key in set(list(existingCollection.keys()) + list(cardsToAdd.keys())):
+        try:
+         d.setdefault(key,[]).append(existingCollection[key])        
+        except KeyError:
+         pass
 
-for i in range (0,15):
-    getName =playerNameGen()
-    getFoil = setFoilStatus()
-    getAuto = setAutoStatus()
-    case ={getName: (getFoil,getAuto)}
-    case_list.append(case)
+        try:
+         d.setdefault(key,[]).append(cardsToAdd[key])          
+        except KeyError:
+         pass
+    return d
 
-for i, v in enumerate(case_list):
-    print (i,v)
+def newPackGen():
+
+
+    #Generates the attributes of each card in the pack. The pack consists of 15 cards, and each card consists of the Player Name and its status or type (Base, Insert, Foil, Autograph)   
+    player_list=[]
+    status_list=[]
+
+    for i in range (0,15):
+        getName =setPlayerName()
+        getStatus = setCardStatus()
+
+        #This ensures that a player only ever shows up once in a pack, and guarantees 15 cards in every pack
+        while getName in player_list:
+            getName = setPlayerName()
+
+        player_list.insert(i, getName)
+        status_list.insert(i, getStatus)
+
+    #This creates the card as a dictionary, with the player name as the key and the card status as its value
+    getPack = dict(zip(player_list, status_list))
+    return getPack
+
+#Sets the user's current card collection to an empty dictionary
+currentCollection=dict()
+
+print("Welcome to Sports Ball Trading Card Pack Opening Simulator!")
+print("Would you like to (O)pen a new pack, (V)iew your collection, or (Q)uit?")
+
+userInput=input()
+userInput=userInput.lower()
+
+while userInput !="q":
+    #Pressing "O" opens a new pack of 15 cards and adds them to the user's collection
+    if userInput=="o":
+        openPack=newPackGen()
+        print ("Here are the cards you pulled: " )
+    
+        for i, (k, v) in enumerate(openPack.items()):
+             print (i,k,":",v)
+        #This is the process for adding the opened cards to the dictionary
+        currentCollection=copy.deepcopy(collectionAdd(currentCollection,openPack))
+    #Pressing "V" views the cards the player has currently collected along with their card types    
+    elif userInput=="v":
+        print ("Here are the players you have collected, along with their variants:")
+        for i, (k, v) in enumerate(currentCollection.items()):
+            print (i,k,":",v)
+    
+
+    print("Would you like to (O)pen a new pack, (V)iew your collection, or (Q)uit?")
+    userInput=input()
+    userInput=userInput.lower()
+
+##TODO: the deepcopy() function seems to add dictionaries as values. For example, if the user collects 3 base cards of the same player, the output will look like this: [[[BASE]]].
+##There may be another way to add the cards to the user's collection each time a pack is opened.
+
